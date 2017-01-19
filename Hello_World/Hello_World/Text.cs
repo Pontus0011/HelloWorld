349fbe9c9af3c228b16346242df8705481ecd0ca
Pontus Luckman <pontus.luckman@gmail.com>
2017-01-19 13:51:46 +0100

    Succesfully wrote Hello World

---
+++ Hello_World/Hello_World/Text.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class Text
+    {
+        public void Run()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
